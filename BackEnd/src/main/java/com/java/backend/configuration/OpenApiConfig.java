e8c3c8668e6cc13e70b98bf2894050e1886eff6d
Hien <minhhien1062@gmail.com>
2025-05-19 00:59:21 +0700

    5

--- BackEnd/src/main/java/com/java/backend/configuration/OpenApiConfig.java
+++ BackEnd/src/main/java/com/java/backend/configuration/OpenApiConfig.java
@@ -1,10 +1,8 @@
 package com.java.backend.configuration;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
@@ -13,35 +11,25 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenApiConfig {
 
-    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
-
     @Bean
-    public OpenAPI openAPI() {
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "bearerAuth";
+        
         return new OpenAPI()
-                .info(apiInfo())
-                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
-                .components(new Components()
-                        .addSecuritySchemes(SECURITY_SCHEME_NAME, 
-                            new SecurityScheme()
-                                .name(SECURITY_SCHEME_NAME)
-                                .type(SecurityScheme.Type.HTTP)
-                                .scheme("bearer")
-                                .bearerFormat("JWT")
-                        )
-                );
-    }
-
-    private Info apiInfo() {
-        return new Info()
+            .info(new Info()
                 .title("Pet Shop API")
-                .description("RESTful API documentation for Pet Shop application")
                 .version("1.0.0")
-                .contact(new Contact()
-                        .name("Your Name")
-                        .email("your.email@example.com")
-                        .url("https://yourwebsite.com"))
-                .license(new License()
-                        .name("License Name")
-                        .url("https://license-url.com"));
+                .description("API documentation for Pet Shop application")
+            )
+            .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+            .components(new Components()
+                .addSecuritySchemes(securitySchemeName,
+                    new SecurityScheme()
+                        .name(securitySchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")
+                )
+            );
     }
 }
